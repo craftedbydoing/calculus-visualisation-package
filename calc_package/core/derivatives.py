de627847cb0c7c7e00c8044ac6db4853cb05db0a
craftedbydoing <craftedbydoing@gmail.com>
2026-08-06 21:27:48 +0200

    Add: secant line calc and plot

--- calc_package/core/derivatives.py
+++ calc_package/core/derivatives.py
@@ -1,5 +1,14 @@
 import numpy as np
 
 
-def calculate_slope(f, a, h):
+def derivative_at_point(f, a):
+    h = 1e-3
     return (f(a+h)-f(a-h))/(2*h)
+
+
+def calculate_slope_secant(f, a, h):
+    return (f(a+h) - f(a))/(h)
+
+
+def get_secant_values(f, x, a, sec_slope):
+    return np.array(f(a) + sec_slope*(x-a))
