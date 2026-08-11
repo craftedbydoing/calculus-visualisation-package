abdc33d0057b8db63c218bb3ed43862630815421
craftedbydoing <craftedbydoing@gmail.com>
2026-08-11 08:12:50 +0200

    Add: baby steps taylor polynom

--- IDEAS.md
+++ IDEAS.md
@@ -8,4 +8,4 @@ Expand: plot/widget eps_delta_explorer, what if L "DNE"?
 Add also widget slider function for just sequence, slider for n_terms, without eps band.
 
 ### Func. module
-Somehow make sure that isolated singularities get plotted in the right way (with "circle" as a "gap" in domain).
+
---
+++ calc_package/core/series.py
@@ -0,0 +1,22 @@
+import numpy as np
+import math
+
+def taylor_values(x, a, deriv, n):
+    """
+    
+    Args:
+
+        x: array of x's where function live
+        a: point of expansion
+        deriv: callable deriv(k, a) -> k-th derivative of f at a
+        n: degree of taylor
+
+    Returns: 
+        array values of T_n(x)
+    """
+
+    result = np.zeros_like(x)
+    for k in range(n+1):
+        result += (deriv(k, a)) * (1/math.factorial(k)) * ((x - a)**k)
+
+    return result
